7c941c02a15f95df134b0214f276ff1dd1d82fcc
Fionn McCarthy <g00414386@atu.ie>
2025-04-09 15:34:13 +0000

    Project complete

--- SG00414386/src/main/java/com/example/demo/exceptions/MechanicInServiceException.java
+++ SG00414386/src/main/java/com/example/demo/exceptions/MechanicInServiceException.java
@@ -5,7 +5,7 @@ public class MechanicInServiceException extends RuntimeException {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	
 	public MechanicInServiceException(String message) {
         super(message);
 	}
